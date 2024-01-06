9bd243616a496e05e09aa1ca031fa8c1e09336dd
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2024-01-06 19:01:01 +0300

    Update 0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,20 +1,14 @@
 #!/usr/bin/env ruby
 # Ruby script to match the word "School" using regular expressions
 
-# Check if an argument is provided
-if ARGV.empty?
-  puts "Usage: #{$PROGRAM_NAME} <input_string>"
-  exit 1
-end
-
 # Get the input string from the command line argument
-input_string = ARGV.join(' ')
+first_argument_string = ARGV[0]
 
 # Define the regular expression
 regex = /School/
 
-# Check for matches in the input string
-matches = input_string.scan(regex)
+# Check for matches in the first argument
+matches = first_argument_string.scan(regex)
 
 # Display the matches
 puts matches.join
