65357d5ed92906c54bd5fecb27c1f69c6cce8b80
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2024-03-26 11:54:53 +0300

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,8 +3,9 @@
 Python script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress.
 """
-import sys
 import requests
+import sys
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
