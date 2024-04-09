5bbf49eaa2e475e0fd871021c41d104cca8e4c22
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-04-09 19:02:44 +0000

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,26 +7,20 @@ import requests
 
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API and returns the number of subscribers for a given subreddit.
+    Queries Reddit API returns number of subscribers for a given subreddit.
 
     Args:
         subreddit: A string representing the subreddit name.
 
     Returns:
-        The number of subscribers for the subreddit, or 0 if the subreddit is invalid.
+        number of subscribers for subreddit, or 0 if the subreddit is invalid.
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0 Isaac'}
-    response = requests.get(url, headers=headers)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
         return data['data']['subscribers']
     else:
         return 0
-
-
-if __name__ == "__main__":
-    # Test cases
-    print(number_of_subscribers("programming"))  # Example valid subreddit
-    print(number_of_subscribers("this_is_a_fake_subreddit"))  # Example invalid subreddit
