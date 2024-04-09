d8502bb2a80015ace417dd20e6b67bf47f741dcd
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-04-09 20:37:36 +0000

    update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,15 +8,9 @@ import requests
 def number_of_subscribers(subreddit):
     """
     Queries Reddit API returns number of subscribers for a given subreddit.
-
-    Args:
-        subreddit: A string representing the subreddit name.
-
-    Returns:
-        number of subscribers for subreddit, or 0 if the subreddit is invalid.
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {'User-Agent': 'My Custom User Agent'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
