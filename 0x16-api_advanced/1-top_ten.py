a853129bb344bfedd1d8e1825876ed11e79a419a
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-04-09 20:44:58 +0000

    update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,16 +8,20 @@ import requests
 
 
 def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/top.json?limit=10"
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code != 200:
         print("None")
         return
+
     data = response.json()
+
     if 'data' not in data or 'children' not in data['data']:
         print("None")
         return
+
     children = data['data']['children']
     for child in children:
         print(child['data']['title'])
