7d1deae9bb8a542bc65e24ce03d138acb286b735
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-04-09 19:14:23 +0000

    task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+queries the Reddit API and prints the titles of the first 10 hot posts
+ listed for a given subreddit.
+ """
+
+import requests
+
+
+def top_ten(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+
+    headers = {'User-Agent': 'Mozilla/5.0'}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        print("None")
+        return
+
+    data = response.json()
+    if 'data' not in data or 'children' not in data['data']:
+        print("None")
+        return
+
+    children = data['data']['children']
+    for child in children:
+        print(child['data']['title'])
