fb2e36eb432e638d17a2709fdeac864e3dca5715
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-04-09 19:45:08 +0000

    update

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -6,12 +6,15 @@ and prints a sorted count of given keywords.
 
 import requests
 
+
 def count_words(subreddit, word_list, hot_list=[], after=None):
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100"
     headers = {'User-Agent': 'Mozilla/5.0'}
     params = {'after': after} if after else {}
 
-    response = requests.get(url, headers=headers, params=params)
+    response = requests.get(url, headers=headers, params=params,
+                             allow_redirects=False)
+
     if response.status_code != 200:
         return None
 
